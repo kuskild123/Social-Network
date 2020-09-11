8d77e24a205a68c37790aaabc75da28ec60f0f9d
kuskild123 <kuskildin666@mail.ru>
2020-09-11 14:38:00 +0300

    Updates

---
+++ precache-manifest.68c0caab39358a0641ac21425f25359a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ebee5f982087e258ba9a150897cb73",
+    "url": "/kuskild123/social/index.html"
+  },
+  {
+    "revision": "fa64c9c35f678547dfd3",
+    "url": "/kuskild123/social/static/css/main.7d39c9e8.chunk.css"
+  },
+  {
+    "revision": "6cc90e9b657347f66388",
+    "url": "/kuskild123/social/static/js/2.ee396b7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kuskild123/social/static/js/2.ee396b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa64c9c35f678547dfd3",
+    "url": "/kuskild123/social/static/js/main.5e0b38c6.chunk.js"
+  },
+  {
+    "revision": "c354573fdba167681f9b",
+    "url": "/kuskild123/social/static/js/runtime-main.9704a8f9.js"
+  }
+]);
